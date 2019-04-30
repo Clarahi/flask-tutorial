408b6deaefbd912282bc3315fb0e70d0d4334349
Clarahi <clara.higuera@bbc.co.uk>
2019-04-30 11:52:10 +0100

    new changes

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,15 @@
 # flask_app.py
 from flask import Flask
+from flask import jsonify
+
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
 @app.route("/")
 def hello():
     return "Hello World! lalalala"
+
+@app.route("/")
+def jsonify():
+	dic={'winter':'invierno','summer':'verano'}
+	return jsonify(data)
